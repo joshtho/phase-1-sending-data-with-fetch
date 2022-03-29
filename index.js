781bce66239fd22618b32095bf0427cc079fe3a8
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2022-03-29 14:06:04 -0500

    commit tree 756cbeb4d2261eed0c7c4d544f6cf8d70f017e11

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    const userObj = {
+        name: `${userName}`,
+        email: `${userEmail}`
+    }
+    const userPost = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify(userObj)
+    }
+    return fetch("http://localhost:3000/users", userPost)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (object) {
+       const p = document.createElement("p");
+        document.body.append(p)
+        p.textContent = object.id
+        return object.id
+    })
+    .catch(function (error) {
+        const p = document.createElement("p");
+        document.body.append(p)
+        p.textContent = error
+        return error
+    })
+}
